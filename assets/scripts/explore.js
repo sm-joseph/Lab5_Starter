0aa2a836dc12de1e9f3983e38686a10a85086068
Sanjit Joseph <smjoseph@ucsd.edu>
2022-10-30 00:55:00 -0700

    Downloads voices and adds 1st to HTML select

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,12 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
+  const voices = synth.getVoices();
+
+  var voice_selection = document.getElementById("voice-select");
+  var option = document.createElement("option");
+  option.text = voices[0].name;
+  voice_selection.add(option);
+
 }
