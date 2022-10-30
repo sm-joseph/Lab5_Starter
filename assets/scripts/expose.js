21654a4fdd8dff1de4b5e7b5075e18958dd4bb2d
Sanjit Joseph <smjoseph@ucsd.edu>
2022-10-29 17:45:21 -0700

    Feature: Image changes when new horn selected

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,32 @@
 // expose.js
 
+// run init once DOM loads
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  // variable for the dropdown that selects type of horn
+  const horn_selector = document.getElementById('horn-select');
+
+  horn_selector.onchange = function() {
+    changeImage(horn_selector.value);
+  }
+
+}
+
+function changeImage(horn_name) {
+  var center_img = document.querySelector('img[alt="No image selected"]');
+  
+  switch(horn_name) {
+    case 'air-horn':
+      center_img.src = "assets/images/air-horn.svg";
+      break;
+    case 'car-horn':
+      center_img.src = "assets/images/car-horn.svg";
+      break;
+    case 'party-horn':
+      center_img.src = "assets/images/party-horn.svg";
+      break;
+  } 
 }
