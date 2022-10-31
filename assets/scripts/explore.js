2e430a8bcc35767be2f196d1f74a6a99acde02f7
Sanjit Joseph <smjoseph@ucsd.edu>
2022-10-30 21:04:10 -0700

    Updated comments and reformatted

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,53 +1,61 @@
 // explore.js
 
+// run init once DOM loads
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+
+  // array of voices to be filled later
   var voices = [];
+
+  // load needed elements from HTML
   const synth = window.speechSynthesis;
   const play_button = document.querySelector("button");
+  const face_img = document.querySelector('img[alt="Smiling face"]');
+  const voice_selection = document.getElementById("voice-select");
+  const inputForm = document.getElementById('text-to-speak');
 
-  var face_img = document.querySelector('img[alt="Smiling face"]');
-  
-
-  var voice_selection = document.getElementById("voice-select");
-
+  // keep loading voices until they're all there
   synth.addEventListener('voiceschanged', () => {
     voices = synth.getVoices();
+
+    // add all voices to selection list
     for (let i = 0; i < voices.length; i++) {
       const option = document.createElement('option');
       option.text = voices[i].name;
-
       voice_selection.add(option);
     }
   });
 
-  var inputForm = document.getElementById('text-to-speak');
-
+  // when button clicked, play audio
   play_button.onclick = function () {
-    
-    //alert(inputForm.value);
+
+    // if no text is in the form, do nothing
+    if (inputForm.value === "") return;
+
+    // create utterThis and get selected option
     const utterThis = new SpeechSynthesisUtterance(inputForm.value);
     const selectedOption = voice_selection.selectedOptions[0].text;
-    
+
+    // if no voice is selected, do nothing
     if (selectedOption === voice_selection[0].text) return;
-    //if (inputForm.value === "") return;
-    
+
+    // find the selected voice and attach it to utterThis
     for (let i = 0; i < voices.length; i++) {
       if (voices[i].name === selectedOption) {
         utterThis.voice = voices[i];
       }
     }
 
+    // open mouth and speak
     face_img.src = "assets/images/smiling-open.png";
     synth.speak(utterThis);
+
+    // once speech is finished, close mouth
     utterThis.addEventListener('end', (event) => {
       face_img.src = "assets/images/smiling.png";
     });
-    
   }
-
 }
 
 
